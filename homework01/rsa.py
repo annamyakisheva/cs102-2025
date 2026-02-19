18703e46b4fdae2851e5b40e0c80558beb534ba2
annamyakisheva <myakisheva.2005@inbox.ru>
2026-02-20 00:20:41 +0300

    Реализована функция поиска НОД

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -33,7 +33,9 @@ def gcd(a: int, b: int) -> int:
     >>> gcd(3, 7)
     1
     """
-    # PUT YOUR CODE HERE
+    while b != 0:
+        a, b = b, a % b
+    return a
     pass
 
 
@@ -55,11 +57,10 @@ def generate_keypair(p: int, q: int) -> tp.Tuple[tp.Tuple[int, int], tp.Tuple[in
         raise ValueError("p and q cannot be equal")
 
     # n = pq
-    # PUT YOUR CODE HERE
+    n = p * q
 
     # phi = (p-1)(q-1)
-    # PUT YOUR CODE HERE
-
+    phi = (p - 1) * (q - 1)
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
 
