2fa066ec80844b92783ad99a0515d27fa4b62ee1
annamyakisheva <myakisheva.2005@inbox.ru>
2026-02-20 01:06:36 +0300

    Исправлены ошибки

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -2,7 +2,7 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ciphertext = ""
     for char in plaintext:
         if char.isalpha():
-            start = ord('A') if char.isupper() else ord('a')
+            start = ord("A") if char.isupper() else ord("a")
             new_char = chr(start + (ord(char) - start + shift) % 26)
             ciphertext += new_char
         else:
@@ -13,7 +13,7 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     plaintext = ""
     for char in ciphertext:
         if char.isalpha():
-            start = ord('A') if char.isupper() else ord('a')
+            start = ord("A") if char.isupper() else ord("a")
             new_char = chr(start + (ord(char) - start - shift) % 26)
             plaintext += new_char
         else:
--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -3,8 +3,8 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     for i, char in enumerate(plaintext):
         if char.isalpha():
             key_char = keyword[i % len(keyword)]
-            shift = ord(key_char.upper()) - ord('A')
-            start = ord('A') if char.isupper() else ord('a')
+            shift = ord(key_char.upper()) - ord("A")
+            start = ord("A") if char.isupper() else ord("a")
             new_char = chr(start + (ord(char) - start + shift) % 26)
             ciphertext += new_char
         else:
@@ -16,8 +16,8 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     for i, char in enumerate(ciphertext):
         if char.isalpha():
             key_char = keyword[i % len(keyword)]
-            shift = ord(key_char.upper()) - ord('A')
-            start = ord('A') if char.isupper() else ord('a')
+            shift = ord(key_char.upper()) - ord("A")
+            start = ord("A") if char.isupper() else ord("a")
             new_char = chr(start + (ord(char) - start - shift) % 26)
             plaintext += new_char
         else:
